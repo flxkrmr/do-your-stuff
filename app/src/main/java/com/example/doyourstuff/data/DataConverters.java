98c5933a3cf9bc69ed3a845aa1221e573f81b654
flxkrmr <felixkramerroki@aol.com>
2019-04-04 21:07:42 +0200

    Make DataConverter package private

--- app/src/main/java/com/example/doyourstuff/data/DataConverters.java
+++ app/src/main/java/com/example/doyourstuff/data/DataConverters.java
@@ -8,24 +8,24 @@ import java.util.Date;
 
 import androidx.room.TypeConverter;
 
-public class DataConverters {
+class DataConverters {
     @TypeConverter
-    public static Date timestampToDate(Long value) {
+    static Date timestampToDate(Long value) {
         return value == null ? null : new Date(value);
     }
 
     @TypeConverter
-    public static Long dateToTimestamp(Date date) {
+    static Long dateToTimestamp(Date date) {
         return date == null ? null : date.getTime();
     }
 
     @TypeConverter
-    public static LocalDateTime timestampToLocalDateTime(Long value) {
+    static LocalDateTime timestampToLocalDateTime(Long value) {
         return value == null ? null : LocalDateTime.ofInstant(Instant.ofEpochSecond(value), ZoneOffset.UTC);
     }
 
     @TypeConverter
-    public static Long localDateTimeToTimestamp(LocalDateTime value) {
+    static Long localDateTimeToTimestamp(LocalDateTime value) {
         return value == null ? null : value.atZone(ZoneOffset.UTC).toEpochSecond();
     }
 }
